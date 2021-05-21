f3241e8d041739d7997a2fee540d503416b9e45e
tanulo <kancsalmate27@gmail.com>
2021-05-21 14:19:55 +0200

    SANYI

--- n_menu_gamestage.py
+++ n_menu_gamestage.py
@@ -9,9 +9,11 @@ class GameStage(MyStage):
     def jerrymove(self, pos, btn):
         animate(self.m, pos=pos)
 
+
+
     def __init__(self, menu: 'Menustage'):
         super().__init__()
-        self.m: MyActor = MyActor("m_jerry.png", pos=(300, 300), anchor=(0, 0))
+        self.m: MyActor = MyActor("auto.png", pos=(600, 150), anchor=(0, 0))
         self.m.set_on_mouse_down_listener(self.back)
         self.add_actor(self.m)
         self.menu: Menustage = menu
